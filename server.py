4f82cf8a4d6d0713cee91bd299ec2a3feda4f541
vinay <vsinghinda@gmail.com>
2022-04-11 12:11:28 +0530

    tkinter

---
+++ server.py
@@ -0,0 +1,52 @@
+import sqlite3
+def connect():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book(id INTEGER PRIMARY KEY,title TEXT,author TEXT,year INTEGER,price INTEGER)")
+    conn.commit()
+    conn.close()
+
+connect()
+
+def insert(title,auth,year,price):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES(NULL,?,?,?,?)",(title,auth,year,price))
+    conn.commit()
+    conn.close()
+
+#insert('manSearch','alex',1995,399)
+
+def view():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
+
+def search(title="",auth="",year="",price=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR price=?",(title,auth,year,price))
+    rows=cur.fetchall()
+    conn.commit()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(title,auth,year,price,id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?,author=?,year=?,price=? WHERE id=?",(title,auth,year,price,id))
+    conn.commit()
+    conn.close()
+
